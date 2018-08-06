4aec57d2df5d10c89a1c2bca282bbf6c590b3980
Ryan James <rejames@gmail.com>
2018-08-06 10:31:29 +0100

    Fixed issue with AddToo component

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
-let AddTodo = ({ dispatch }) => {
+const AddTodo = ({ dispatch }) => {
   let input
 
   return (
@@ -26,6 +26,4 @@ let AddTodo = ({ dispatch }) => {
   )
 }
 
-AddTodo =  connect()(AddTodo)
-
-export default AddTodo
+export default connect()(AddTodo)
